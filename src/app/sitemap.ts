4b79520fc45d4377b5125577391c6c869a0de6f7
clinsmann <ibeanuhillary@gmail.com>
2026-02-08 16:09:29 +0100

    Update sitemap configuration to enforce static generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { websiteUrl } from '@/lib/conts'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
